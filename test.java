4ec00a763a865eeb96e8e65c7d2a737d4d1d5454
SangNguyen <nngocsang38@gmail.com>
2022-10-30 14:11:34 +0700

    thu nghiem git, them vao file test

---
+++ test.java
@@ -0,0 +1,8 @@
+import javax.sound.midi.MidiChannel;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("Dang hoc git");
+    }
+}
